611cd4ba02a69ece5a812fdd28430c496845917d
KeshavDeploys <keshavdeploys@gmail.com>
2026-02-14 19:33:29 +0530

    clean repo for fresh pipeline test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Push2Prod is LIVE, , and hi i am Push2Prod author Keshav :) and i welcome you to Push2Prod and this is a demo test well well well. and my co founder is Aditya Singh.");
+  res.end("Push2Prod is LIVE, , and hi i am Push2Prod author Keshav :) and i welcome you to Push2Prod and this is a demo test well well well. and my co founder isAditya Singh");
 });
 
 server.listen(3000, () => {
